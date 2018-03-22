fad78cd26318f526378e17892b0a0208e9203141
Evan Kesten <ebk46@cornell.edu>
2018-03-22 12:24:58 -0400

    forgot to change config back to remove CozeServer

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  cozeHost: "http://middleware.ddns.net:5000",
-  //cozeHost: "http://bearnet.dyndns.org:5000",
+  //cozeHost: "http://middleware.ddns.net:5000",
+  cozeHost: "http://bearnet.dyndns.org:5000",
 };
